ac9155599c2c25b7ca669bcac1317ce265a60b78
Egor Boldyrev <Egor.Boldyrev@noveogroup.com>
2019-10-24 02:09:34 +0700

    Initial commit with empty project

---
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.nsu.fit.g16201.kinopoisklite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
