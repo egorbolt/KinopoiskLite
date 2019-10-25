b8e707ad1147fd7e17c884c58779b8e742f3148b
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-25 20:48:28 +0700

    Добавлено хоть какое-то изменение чего-то при изменении выбранного пункта панели навигации (switch case) + новые более подходящие иконки + изменение цвета иконок при выборе пункта

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/MainActivity.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +17,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        final TextView textView = findViewById(R.id.text_view);;
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.action_explore:
+                                //todo: сделать видимым какой-нибудь из других activity
+                                textView.setText("Explore");
+                                break;
+                            case R.id.action_random:
+                                textView.setText("Random");
+                                break;
+                            case R.id.action_lists:
+                                textView.setText("Lists");
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 }
