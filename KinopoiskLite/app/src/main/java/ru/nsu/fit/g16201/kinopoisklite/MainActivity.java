d03befe9f25003276876a5ee9eb89ca102890e15
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-25 20:10:41 +0700

    Добавлена панель навигации снизу

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/MainActivity.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.nsu.fit.g16201.kinopoisklite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
