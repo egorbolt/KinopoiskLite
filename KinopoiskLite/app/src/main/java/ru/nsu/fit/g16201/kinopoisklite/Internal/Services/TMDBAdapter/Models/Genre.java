a67b04981c4064b8dc07d8cc07dd3fd73bf2963b
pda898 <pda00898@gmail.com>
2019-11-10 16:58:44 +0700

    Добавлен класс модели для жанров

---
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/Services/TMDBAdapter/Models/Genre.java
@@ -0,0 +1,22 @@
+package ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models;
+
+public class Genre {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
