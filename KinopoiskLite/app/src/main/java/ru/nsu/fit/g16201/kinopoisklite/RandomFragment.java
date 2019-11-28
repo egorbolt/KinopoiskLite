adf7b4508a9e541711ccac45c9aaf0f213aade9a
Egor Boldyrev <Egor.Boldyrev@noveogroup.com>
2019-11-28 23:37:02 +0700

    Добавил картинку и текст на экран, начал подгрузку жанров

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/RandomFragment.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/RandomFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RandomFragment extends Fragment {
+    private TextView movieDescription;
+    private ImageView moviePoster;
+
     public RandomFragment() {
     }
 
@@ -17,6 +22,12 @@ public class RandomFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_random, container, false);
+
+        movieDescription = view.findViewById (R.id.rmDescription);
+        moviePoster = view.findViewById(R.id.rmImageView);
+
+
+
         return view;
     }
 
