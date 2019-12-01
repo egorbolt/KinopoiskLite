c2a883891f38c9b2f88181d987507bdeb5fa8389
Denis Migranov <d.migranov@g.nsu.ru>
2019-12-01 16:43:53 +0700

    реализован конструктов для Литсенера нажатия на элемент списка

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/RecyclerItemClickListener.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/RecyclerItemClickListener.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.g16201.kinopoisklite;
 
 import android.content.Context;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
 
 
+    private OnItemClickListener mListener;
+
+    GestureDetector mGestureDetector;
+
     public interface OnItemClickListener {
         void onItemClick(View view, int position);
 
@@ -18,9 +23,23 @@ class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
     }
 
     public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnItemClickListener listener) {
+        mListener = listener;
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                if (child != null && mListener != null) {
+                    mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child));
+                }
+            }
+        });
 
     }
-
     @Override
     public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
         return false;
