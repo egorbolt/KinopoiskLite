08a59b6376b13c488903c5cceeedf647f009903b
Denis Migranov <d.migranov@g.nsu.ru>
2019-12-06 15:59:07 +0700

    начато добавление загрузки списков с сервера

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/fragments/tabfragments/ExploreFragment.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/fragments/tabfragments/ExploreFragment.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.g16201.kinopoisklite.fragments.tabfragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,6 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.net.MalformedURLException;
+import java.util.function.BiFunction;
+
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.API;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.PagedMovieListTask;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.Movie;
 import ru.nsu.fit.g16201.kinopoisklite.MainActivity;
 import ru.nsu.fit.g16201.kinopoisklite.MovieRatingsAdapter;
@@ -40,19 +45,34 @@ public class ExploreFragment extends Fragment {
         String[] popularDataSet = {
                 "5.6", "7.8", "4.5", "8.6", "5.5"
         };
-        configureMovieCollection(R.id.popular_movie_collection, popularDataSet, "Popular");
+        configureMovieCollection(R.id.popular_movie_collection, popularDataSet, "Popular", (page, language) -> {
+            try {
+                return API.loadPopularList(page, language);
+            } catch (MalformedURLException e) {
+                Log.e("ExploreFragment","Malformed URL Exception: "+ e.getMessage());
+            }
+            return null;
+        });
 
-        String[] anotherDataSet = {
+        String[] trendingDataSet = {
                 "6.6", "1.8", "4.5", "8.4", "5.7"
         };
-        configureMovieCollection(R.id.another_movie_collection, anotherDataSet, "Another");
+        configureMovieCollection(R.id.trending_movie_collection, trendingDataSet, "Trending", (page, language) -> {
+            try {
+                return API.loadTrendingList(page, language);
+            } catch (MalformedURLException e) {
+                Log.e("ExploreFragment", "Malformed URL Exception: " + e.getMessage());
+            }
+            return null;
+        });
 
 
         return view;
     }
 
 
-    private void configureMovieCollection(int id, String[] dataSet, String name)
+
+    private void configureMovieCollection(int id, String[] dataSet, String name, BiFunction<Integer, String, PagedMovieListTask> exploreFragment)
     {
         View movieCollection = view.findViewById(id);
 
@@ -61,13 +81,9 @@ public class ExploreFragment extends Fragment {
 
         MaterialButton button = movieCollection.findViewById(R.id.show_button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ShowAllFragment showAllFragment = ShowAllFragment.newInstance("abc");      //todo: передавать что-то, что опзволит получить нунные фильмы
-                notifyMainActivityFragmentIsActive(showAllFragment);
-            }
+        button.setOnClickListener(v -> {
+            ShowAllFragment showAllFragment = ShowAllFragment.newInstance("abc");      //todo: передавать что-то, что опзволит получить нунные фильмы
+            notifyMainActivityFragmentIsActive(showAllFragment);
         });
 
         RecyclerView recyclerView = movieCollection.findViewById(R.id.recycler_view);
