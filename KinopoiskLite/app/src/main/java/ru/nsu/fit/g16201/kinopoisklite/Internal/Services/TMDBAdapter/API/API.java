be04b9637fcf327a929c341c32788b152feddbcb
pda898 <pda00898@gmail.com>
2019-12-09 17:52:17 +0700

    Метод для получения информации по человеку

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/Services/TMDBAdapter/API/API.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/Services/TMDBAdapter/API/API.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.GenresListTask;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.MovieInfoTask;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.PagedMovieListTask;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.PersonInfoTask;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.Genre;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.GenreList;
 
@@ -73,6 +74,13 @@ public class API {
     public static PagedMovieListTask loadMoviesByGenre(int page, int genre, String language) throws MalformedURLException {
         URL url = new URL(UrlConstructor.urlMoviesListByGenre(page,genre,language));
         PagedMovieListTask task = new PagedMovieListTask();
+        task.execute(url);
+        return task;
+    }
+
+    public static PersonInfoTask loadPersonInfo(int id, String language) throws MalformedURLException {
+        URL url = new URL(UrlConstructor.urlPersonDetailedInfo(id, language));
+        PersonInfoTask task = new PersonInfoTask();
         task.execute(url);
         return task;
     }
