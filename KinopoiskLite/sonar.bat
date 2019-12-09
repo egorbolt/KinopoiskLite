8c1ac3c3cf95b460cdac411097a27d3b472a3763
Denis Migranov <d.migranov@g.nsu.ru>
2019-12-09 21:29:07 +0700

    завершена настройка Сонара (см. батник sonar.bat)

---
+++ KinopoiskLite/sonar.bat
@@ -0,0 +1,2 @@
+set JAVA_HOME="D:\Applications\Android\Android Studio\jre"
+./gradlew sonarqube -Dsonar.projectKey=KL -Dsonar.host.url=http://84.237.50.237:9000 -Dsonar.login=eb67dfe88f699ce908db2c43a7836d59d4504c24
