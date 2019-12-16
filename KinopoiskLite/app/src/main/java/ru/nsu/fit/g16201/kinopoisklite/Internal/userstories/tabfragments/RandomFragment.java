a49c23bfc9f23b4e7188ae1362045e6a544bbe89
Denis Migranov <d.migranov@g.nsu.ru>
2019-12-16 18:05:52 +0700

    загрузка инфо о случайном ифльме

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/userstories/tabfragments/RandomFragment.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/userstories/tabfragments/RandomFragment.java
@@ -22,14 +22,17 @@ import java.util.concurrent.ExecutionException;
 
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.API;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.GenresListTask;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.PagedMovieListTask;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.Genre;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.GenreList;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.Movie;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.Models.PopularMovies;
 import ru.nsu.fit.g16201.kinopoisklite.Internal.userstories.commonfragments.showallfragment.ShowAllFragment;
 import ru.nsu.fit.g16201.kinopoisklite.R;
 
 public class RandomFragment extends Fragment {
     private static final String ERROR_TAG = "RandomFragment";
-    private TextView movieDescription;
+    private TextView movieTitle;
     private ImageView moviePoster;
     private MaterialButton button;
     public RandomFragment() {
@@ -40,14 +43,13 @@ public class RandomFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_random, container, false);
 
-        movieDescription = view.findViewById (R.id.rmDescription);
+        movieTitle = view.findViewById (R.id.rmDescription);
         moviePoster = view.findViewById(R.id.rmImageView);
         MaterialButton button = view.findViewById(R.id.next_button);
         button.setTag("showAllButtonRandom");
         button.setOnClickListener(v -> {
             loadRandom();
         });
-
 
         loadRandom();
 
@@ -105,13 +107,21 @@ public class RandomFragment extends Fragment {
                 List<Genre> genres = genreList.getList();
 
                 Genre genre = genres.get(new Random().nextInt(genres.size()));
-                System.out.println("HELLO" + genre.getName());
+                PagedMovieListTask moviesTask = API.loadMoviesByGenre(new Random().nextInt(10), genre.getId(), "en-US");
+                PopularMovies movies = moviesTask.get();
+                List<Movie> movieList = movies.getResults();
+
+                Movie movie = movieList.get(new Random().nextInt(movieList.size()));
+                System.out.println("dfsfdfsdffsJJDS: " + movie.getTitle());
+
 
             } catch (ExecutionException e) {
                 Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
             } catch (InterruptedException e) {
                 Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
                 Thread.currentThread().interrupt();
+            } catch (MalformedURLException e) {
+                Log.e(ERROR_TAG, "Malformed URL" + e.getMessage());
             }
         }
 
