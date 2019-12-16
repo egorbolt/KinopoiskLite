b38e24e92d34155d8be35adb7146c3f9c616a4f4
Denis Migranov <d.migranov@g.nsu.ru>
2019-12-16 17:31:55 +0700

    asynctask для загрузки жанров начат

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/userstories/tabfragments/RandomFragment.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/Internal/userstories/tabfragments/RandomFragment.java
@@ -2,6 +2,7 @@ package ru.nsu.fit.g16201.kinopoisklite.Internal.userstories.tabfragments;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
+
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.API;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.Services.TMDBAdapter.API.Tasks.GenresListTask;
+import ru.nsu.fit.g16201.kinopoisklite.Internal.userstories.commonfragments.showallfragment.ShowAllFragment;
 import ru.nsu.fit.g16201.kinopoisklite.R;
 
 public class RandomFragment extends Fragment {
+    private static final String ERROR_TAG = "RandomFragment";
     private TextView movieDescription;
     private ImageView moviePoster;
-
+    private MaterialButton button;
     public RandomFragment() {
     }
 
@@ -28,23 +38,50 @@ public class RandomFragment extends Fragment {
 
         movieDescription = view.findViewById (R.id.rmDescription);
         moviePoster = view.findViewById(R.id.rmImageView);
-
-
+        MaterialButton button = view.findViewById(R.id.next_button);
+        button.setTag("showAllButtonRandom");
+        button.setOnClickListener(v -> {
+            //todo
+        });
 
         return view;
     }
 
-    class DownloadGenres extends AsyncTask<String, Void, Integer> {
+    class DownloadGenres extends AsyncTask<String, Void, Void> {
         @Override
-        protected Integer doInBackground(String... parameter) {
-            return 0;
+        protected Void doInBackground(String... parameter) {
+            GenresListTask genresListTask = null;
+            try {
+                genresListTask = API.loadGenreList("en-US");
+            } catch (MalformedURLException e) {
+                Log.e(ERROR_TAG, "Malformed URL" + e.getMessage());
+            }
+            if(genresListTask != null)
+            {
+                try {
+                    genresListTask.get();
+                } catch (ExecutionException e) {
+                    Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
+                } catch (InterruptedException e) {
+                    Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            return null;
         }
 
-        @Override
+        /*@Override
         protected void onPostExecute(Integer result) {
             // [... Сообщите о результате через обновление пользовательского
             // интерфейса, диалоговое окно или уведомление ...]
-        }
+        }*/
+    }
+
+
+    public void loadRandom() throws MalformedURLException {
+
+
     }
 
     //todo: newInstance?
