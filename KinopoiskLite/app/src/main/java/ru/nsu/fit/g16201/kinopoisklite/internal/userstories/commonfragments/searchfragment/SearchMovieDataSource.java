38d185d8eeb7ae19d65e6caf7bf2a95bfd406e62
Egor Boldyrev <Egor.Boldyrev@noveogroup.com>
2019-12-20 10:23:48 +0700

    Взял за основу другой фрагмент

--- KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/internal/userstories/commonfragments/searchfragment/SearchMovieDataSource.java
+++ KinopoiskLite/app/src/main/java/ru/nsu/fit/g16201/kinopoisklite/internal/userstories/commonfragments/searchfragment/SearchMovieDataSource.java
@@ -36,25 +36,25 @@ public class SearchMovieDataSource extends PageKeyedDataSource<Integer, Movie> {
 
     @Override
     public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull LoadInitialCallback<Integer, Movie> callback) {
-        List<Movie> dataSet = new ArrayList<>();
-        if(task != null) {
-            try
-            {
-                PopularMovies movies = task.get();
-                if(movies != null)
-                    dataSet = movies.getResults();
-            }
-            catch (InterruptedException e)
-            {
-                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
-                Thread.currentThread().interrupt();
-            }
-            catch (ExecutionException e)
-            {
-                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
-            }
-        }
-        callback.onResult(dataSet, null, 2);
+//        List<Movie> dataSet = new ArrayList<>();
+//        if(task != null) {
+//            try
+//            {
+//                PopularMovies movies = task.get();
+//                if(movies != null)
+//                    dataSet = movies.getResults();
+//            }
+//            catch (InterruptedException e)
+//            {
+//                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
+//                Thread.currentThread().interrupt();
+//            }
+//            catch (ExecutionException e)
+//            {
+//                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
+//            }
+//        }
+//        callback.onResult(dataSet, null, 2);
     }
 
     @Override
@@ -64,35 +64,35 @@ public class SearchMovieDataSource extends PageKeyedDataSource<Integer, Movie> {
 
     @Override
     public void loadAfter(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, Movie> callback) {
-        int page = params.key;
-
-        PagedMovieListTask pageTask;
-        try {
-            pageTask = PagedListLoader.loadParametrisedList(type, page, id,"en-US");
-        } catch (MalformedURLException e) {
-            Log.e(ERROR_TAG, "Malformed URL" + e.getMessage());
-            return;
-        }
-
-        List<Movie> dataSet = new ArrayList<>();
-        if(pageTask != null) {
-            try
-            {
-                PopularMovies movies = pageTask.get();
-                if(movies != null)
-                    dataSet = movies.getResults();
-            }
-            catch (InterruptedException e)
-            {
-                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
-                Thread.currentThread().interrupt();
-            }
-            catch (ExecutionException e)
-            {
-                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
-            }
-        }
-        callback.onResult(dataSet, params.key+1);
+//        int page = params.key;
+//
+//        PagedMovieListTask pageTask;
+//        try {
+//            pageTask = PagedListLoader.loadParametrisedList(type, page, id,"en-US");
+//        } catch (MalformedURLException e) {
+//            Log.e(ERROR_TAG, "Malformed URL" + e.getMessage());
+//            return;
+//        }
+//
+//        List<Movie> dataSet = new ArrayList<>();
+//        if(pageTask != null) {
+//            try
+//            {
+//                PopularMovies movies = pageTask.get();
+//                if(movies != null)
+//                    dataSet = movies.getResults();
+//            }
+//            catch (InterruptedException e)
+//            {
+//                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
+//                Thread.currentThread().interrupt();
+//            }
+//            catch (ExecutionException e)
+//            {
+//                Log.e(ERROR_TAG, "Can't retrieve data: " + e.getMessage());
+//            }
+//        }
+//        callback.onResult(dataSet, params.key+1);
 
     }
 }
